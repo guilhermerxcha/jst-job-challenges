f167b9c2d100a81a36f81cf26474e49351bb4afe
Guilherme <guilherme.rocha@temasistemas.com.br>
2021-08-21 18:31:49 -0300

    inicio

---
+++ back-end/pokedexjusta/src/main/java/com/guilherme/pokedexjusta/PokedexjustaApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.pokedexjusta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexjustaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexjustaApplication.class, args);
+	}
+
+}
---
+++ back-end/pokedexjusta/src/test/java/com/guilherme/pokedexjusta/PokedexjustaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.pokedexjusta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexjustaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
