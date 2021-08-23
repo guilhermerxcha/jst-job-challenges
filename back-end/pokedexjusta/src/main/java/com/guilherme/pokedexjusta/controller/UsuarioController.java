5f356cbd46c5a39fb96aa5d99c2a15e1e5237910
Guilherme <guilherme.rocha@temasistemas.com.br>
2021-08-23 20:20:48 -0300

    aplicando swagger

--- back-end/pokedexjusta/src/main/java/com/guilherme/pokedexjusta/controller/UsuarioController.java
+++ back-end/pokedexjusta/src/main/java/com/guilherme/pokedexjusta/controller/UsuarioController.java
@@ -2,46 +2,92 @@ package com.guilherme.pokedexjusta.controller;
 
 import com.guilherme.pokedexjusta.dto.UsuarioDTO;
 import com.guilherme.pokedexjusta.service.UsuarioService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class UsuarioController {
 
     @Autowired
     UsuarioService usuarioService;
 
+    @ApiOperation(value = "Cria um novo usuario.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Cria um novo usuario."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @PostMapping(value = "/novousuario")
     public UsuarioDTO criarNovoUsuario(@RequestBody UsuarioDTO usuarioDTO) {
         return this.usuarioService.criarNovoUsuario(usuarioDTO);
     }
 
+    @ApiOperation(value = "Edita usuario existente.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Edita usuario existente."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @PutMapping(value = "/editarusuario")
     public UsuarioDTO editarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
         return this.usuarioService.editarUsuario(usuarioDTO);
     }
 
+    @ApiOperation(value = "Deleta usuario pelo id.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Deleta usuario pelo id."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @DeleteMapping(value = "/{id}")
     public void deletarUsuario( @PathVariable int id) {
         this.usuarioService.deletarUsuarioPorId(id);
     }
 
+    @ApiOperation(value = "Retorna a lista de usuarios.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna a lista de usuarios."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @GetMapping(value = "/obtertodosusuarios")
     public List<UsuarioDTO> buscarTodos() {
         return this.usuarioService.obterTodosUsuarios();
     }
 
+    @ApiOperation(value = "Obtem usário pelo id.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Obtem usário pelo id."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @GetMapping(value = "/{id}")
     public UsuarioDTO obterUsuarioPorId(@PathVariable int id) {
         return this.usuarioService.obterUsuarioPorId(id);
     }
 
+    @ApiOperation(value = "Retorna usuario a partir do email na busca.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna usuario a partir do email na busca."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @GetMapping(value = "/{email}")
     public UsuarioDTO obterUsuarioPorEmail(@PathVariable String email) {
         return this.usuarioService.obterUsuarioPorEmail(email);
     }
 
+    @ApiOperation(value = "Deleta todos os usuários.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Deleta todos os usuários."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @DeleteMapping(value = "/deletartodos")
     public void deletarTodosUsuarios() {
         this.usuarioService.deletarTodosUsuarios();
---
+++ back-end/pokedexjusta/src/main/java/com/guilherme/pokedexjusta/swagger/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package com.guilherme.pokedexjusta.swagger;
+
+import io.swagger.models.auth.In;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.guilherme.pokedexjusta.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+                .securityContexts(Arrays.asList(securityContext()));
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope;
+        authorizationScope = new AuthorizationScope("ADMIN", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(
+                new SecurityReference("Token Access", authorizationScopes));
+    }
+
+}
